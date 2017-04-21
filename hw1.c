69d5c00ee14dbcd1e1c14dff12fed93d5fd81b4d
yogitapgarud <garudyogitap@gmail.com>
2017-04-21 15:29:34 -0700

    Solution with 1 producer and multiple consumer
    Fixed the synchronization errors between multiple consumers

--- hw1.c
+++ hw1.c
@@ -12,6 +12,7 @@
 
 #define BUFFER_SIZE 32
 
+int get_random_number();
 void *producer();
 void *consumer();
 
@@ -27,7 +28,7 @@ struct shared buffer[BUFFER_SIZE];
 
 int in = 0;
 int out = 0;
-
+int counter = 0;
 
 int main(int argc, char **argv)
 {
@@ -41,22 +42,30 @@ int main(int argc, char **argv)
 			NULL);
 
 	printf("created thread producer \n");
-	pthread_create(&threads[1],
-			NULL,
-			consumer,
-			NULL);
 
-	printf("created thread consumer \n");
+	for(int j = 1; j < total_threads; j++)
+	{
+		pthread_create(&threads[j],
+				NULL,
+				consumer,
+				NULL);
 
-	pthread_join(threads[0], NULL);
-	pthread_join(threads[1], NULL);
+		printf("created thread consumer %d \n", j);
+	}
+
+	//pthread_join(threads[0], NULL);
+
+	for(int j = 0; j < total_threads; j++)
+	{
+		pthread_join(threads[j], NULL);
+	}
 
 	printf("joined threads \n");
 	
 	return 0;
 }
 
-int get_random_number()
+int get_random_number(void)
 {
 	unsigned int eax;
 	unsigned int ebx;
@@ -85,7 +94,8 @@ int get_random_number()
 		asm volatile ("rdrand %0; setc %1"
 			: "=r" (rand), "=qm" (ok));
 
-		random_number = 3  + rand % 5;
+		//random_number = 3  + rand % 5;
+		random_number = rand;
 		printf("after getting random \n");
 		printf("%" PRIu32 "\n", random_number);
 
@@ -94,56 +104,76 @@ int get_random_number()
 	}
 	else{
 		//use mt19937
-		printf("RdRand support is not present \n");
+		printf("RdRand support is not present, using MERSENNE TWISTER \n");
 
 		gettimeofday(&tv,NULL);
-		unsigned long time_in_micros = 1000000 * tv.tv_sec + tv.tv_usec;
+		unsigned long time_in_micros = 1000 * tv.tv_sec + tv.tv_usec;
 		init_genrand((ulong)time_in_micros);
-		random_number = 3 + (genrand_int32() % (5));	//output = min + (rand() % (int)(max - min + 1)) in specific range
+		//random_number = 3 + (genrand_int32() % (5));	//output = min + (rand() % (int)(max - min + 1)) in specific range
+		random_number = genrand_int32() % 1000;
+		printf("random number : %d \n \n", random_number);
 	}
 	return random_number;
 }
 
 void *producer()
 {
-//	int in = 0;
-	//struct args *a = (struct args*)tid;
+	int random = 0;
 	printf("In producer \n");
 	while(1) {
-		while (((in + 1) % BUFFER_SIZE) == out);	
-		printf("random sleep time for producer: %d \n", prod_sleep_time);
+		while (counter == BUFFER_SIZE) ;	
 
-		sleep(get_random_number);
+		random = 2 + (int)get_random_number() % 8;
+		printf("random sleep time for producer: %d \n", random);
+		sleep(random);
 
 		pthread_mutex_lock(&mutex);
 		printf("locked cs in producer\n");
-		buffer[in].num = get_random_number;
+		random = (int)get_random_number() % 1000;
+		buffer[in].num = random;
 		printf("random num generated from producer: %d \n", buffer[in].num);
 
-		buffer[in].sleep_time = get_random_number; //2 + (genrand_int32() % 8);
+		random = 3 + (int)get_random_number() % 5;
+		buffer[in].sleep_time = random; //2 + (genrand_int32() % 8);
 		printf("random num generated from producer: %d \n", buffer[in].sleep_time);
 
 		in = (in+1) % BUFFER_SIZE;
+		counter++;
+		printf("counter : %d \n \n", counter);
 		pthread_mutex_unlock(&mutex);
 	}
 }
 
 void *consumer()
 {
-//	int out = 0;
-	printf("In consumer \n");
-	while(1) {
-		//pthread_mutex_lock(&mutex);
-		while(in == out);
+	/*pthread_id_np_t tid;
+	tid = pthread_getthreadid_np();*/
+	pthread_t ptid = pthread_self();
+	printf("In consumer %ld \n", ptid);
 
+	while(1) {
+
+		while(counter == 0) ;
+		
 		pthread_mutex_lock(&mutex);
-		printf("locked cs in consumer \n");
+		if (counter == 0)
+			pthread_mutex_unlock(&mutex);
+		else
+		{ 
+		pthread_t ptid = pthread_self();
+	        //printf("In consumer %ld \n", ptid);
+		printf("locked cs in consumer %ld and out = %d \n", ptid, out);
+		printf("counter before decreasing: %d \n \n", counter);
 		printf("consumer will be sleeping for %d time \n", buffer[out].sleep_time);
 		sleep(buffer[out].sleep_time);
 		int consumed = buffer[out].num;
 		out = (out + 1) % BUFFER_SIZE;
+		counter--;
+		printf("counter consumer : %d \n \n", counter);
 		printf("consumed value: %d \n", consumed);
 
 		pthread_mutex_unlock(&mutex);
+		}
+		printf("released lock \n \n");
 	}
 }
